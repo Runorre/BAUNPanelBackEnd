ae2dd194563e4de768249179590d5a0cfc27ee48
Runorre <angelo.anselmet@epitech.eu>
2025-06-03 04:22:21 +0200

    🏷️ Add: Introduce StateType enum for IYouga status and update interface to use the new enum

--- src/types/database/IYouga.ts
+++ src/types/database/IYouga.ts
@@ -1,9 +1,10 @@
 import { Types } from 'mongoose';
+import { StateType } from '../enum/enumStateType';
 
 export interface IYouga {
     _id: Types.ObjectId;
     name: string;
     color: string;
     waste : number;
-    status : any; //TODO Made a Enum for the status, refer to the Specifications to know the list
+    status : StateType;
 }
---
+++ src/types/enum/enumStateType.ts
@@ -0,0 +1,11 @@
+export enum StateType {
+    REPARE = "À réparer",
+    EMPTY = "À vider",
+    OIL = "Voyant huile",
+    FUEL = "Plein à faire",
+    EVENT = "Evenement",
+    JAIL = "Fourrière",
+    UNICORN = "Unicorn",
+    CLEAN = "À nettoyer",
+    OUT = "Ne pas sortir"
+}
